f4904f9b901ec0ef2039e632795d1217c7b8d358
Celio Oda Moretti <comoretti@gmail.com>
2023-12-02 18:46:09 -0300

    alterado tema padrao

--- chadrc.lua
+++ chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "onenord",
-  theme_toggle = { "onedark", "onenord" },
+  theme = "jellybeans",
+  theme_toggle = { "onedark", "jellybeans" },
 
   hl_override = highlights.override,
   hl_add = highlights.add,
